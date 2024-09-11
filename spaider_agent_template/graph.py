cf90314517613f4c5f11d086c1efc9ba83adc8c7
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2024-09-11 13:54:38 +0530

    recreated the graph adding nodes fs_manager and tools.

--- spaider_agent_template/graph.py
+++ spaider_agent_template/graph.py
@@ -1,8 +1,6 @@
 from langgraph.graph import StateGraph, END
+from langgraph.constants import END
 from typing import TypedDict, Annotated, List, Dict, Any
-from spaider_agent_template.nodes_and_cond_edges import *
-from spaider_agent_template.tools import *
-from spaider_agent_template.schemas import AgentState
 import sqlite3
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_core.messages import HumanMessage, AIMessage
@@ -10,79 +8,56 @@ from langgraph.prebuilt import ToolNode
 import subprocess
 from dotenv import load_dotenv
 
+'''LOCAL IMPORTS'''
+from spaider_agent_template.nodes_and_cond_edges import *
+from spaider_agent_template.tools import *
+from spaider_agent_template.schemas import State
+
+
 load_dotenv()
 
-# Use a context manager for the SqliteSaver
-with SqliteSaver.from_conn_string(":memory:") as memory:
+'''GRAPH INSTANCE'''
+builder = StateGraph(State)
 
-    '''GRAPH INSTANCE'''
-    builder = StateGraph(AgentState)
+'''LIST OF NODES TO THE GRAPH'''
+builder.add_node("fs_manager", fs_manager_node)
+builder.add_node("tools", tool_node)
 
-    '''LIST OF NODES TO THE GRAPH'''
-    builder.add_node("fs_manager", fs_manager_node)
-    builder.add_node("query", query_node)
-
-    '''SET AN ENTRY POINT FOR THE GRAPH'''
-    builder.set_entry_point("query")
+'''SET AN ENTRY POINT FOR THE GRAPH'''
+builder.set_entry_point("fs_manager")
 
 
-    '''ADD EDGES/CONDITIONAL EDGES FOR THE GRAPH'''
-    '''SYNTAX FOR CONDITIONAL EDGES, COPY THIS.'''
-    # builder.add_conditional_edges(
-    #     "generate", 
-    #     should_continue, 
-    #     {END: END, "reflect": "reflect"}
-    # )
+'''ADD EDGES/CONDITIONAL EDGES FOR THE GRAPH'''
+'''SYNTAX FOR CONDITIONAL EDGES, COPY THIS.'''
+builder.add_conditional_edges("agent", should_continue, {"continue": "tools", "end": END})
 
-    '''SYNTAX FOR REGULAR EDGES, COPY THIS.'''
-    builder.add_edge("query", "fs_manager")
-    builder.add_edge("fs_manager", "query")
-    # builder.add_edge("node1name", "node2name")
-    # builder.add_edge("fs_manager", "fs_manager") # edge from fs_manager to itself, essentially looping its function.
+'''SYNTAX FOR REGULAR EDGES, COPY THIS.'''
+builder.add_edge("tools", "fs_manager")
 
-    graph = builder.compile(checkpointer=memory)
+graph = builder.compile()
+    
+    
+# Create a thread configuration
+thread = {"configurable": {"thread_id": "1"}}
+task = input("User >>")
+for s in graph.stream(
+    {
+        'task': task,
+        'current_directory_path': (subprocess.run("pwd", shell=True, check=True, text=True, capture_output=True)).stdout
+    },
+    thread
+):
+    for key, value in s.items():
+        print(f"{key}:")
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                print(f"  {sub_key}:")
+                print(f"    {sub_value}")
+                print()
+        else:
+            print(value)
+        print()
         
-        
-    # Create a thread configuration
-    thread = {"configurable": {"thread_id": "1"}}
-    task = input("User >>")
-    for s in graph.stream(
-        {
-            'task': task,
-            'current_directory_path': (subprocess.run("pwd", shell=True, check=True, text=True, capture_output=True)).stdout
-        },
-        thread
-    ):
-        for key, value in s.items():
-            print(f"{key}:")
-            if isinstance(value, dict):
-                for sub_key, sub_value in value.items():
-                    print(f"  {sub_key}:")
-                    print(f"    {sub_value}")
-                    print()
-            else:
-                print(value)
-            print()
-        
-    # # Main execution loop
-    # while True:
-    #     task = input("User >> ")
-    #     state = {
-    #         'task': task,
-    #         'current_directory': '/',  # Set an initial directory
-    #         'file_tree': '',
-    #         'recent_actions': [],
-    #         'error_messages': [],
-    #     }
-
-    #     for s in graph.stream(state):
-    #         if s.get('changes'):
-    #             print("Assistant:", s['changes'])
-            
-    #         if s == END:
-    #             print("Goodbye!")
-    #             exit(0)
-
-    #     print()  # Add a blank line for readability between interactions
+   
     
 
